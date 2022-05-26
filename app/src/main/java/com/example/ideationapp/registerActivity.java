757490a95c9ae156fb7f3cc1040d06916e68c59b
tagrawal996 <tanyagra00@gmail.com>
2022-05-27 01:59:08 +0530

    register Page created

--- app/src/main/java/com/example/ideationapp/registerActivity.java
+++ app/src/main/java/com/example/ideationapp/registerActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class registerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 
+    public void LoginPage(View view) {
+    }
 }
