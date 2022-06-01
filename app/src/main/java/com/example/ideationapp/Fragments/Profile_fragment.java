69e8cb54d4bd8da1fdd559b70c3c100d5ec0ac30
tagrawal996 <tanyagra00@gmail.com>
2022-06-01 20:07:26 +0530

    profile photo added

--- app/src/main/java/com/example/ideationapp/Fragments/Profile_fragment.java
+++ app/src/main/java/com/example/ideationapp/Fragments/Profile_fragment.java
@@ -1,11 +1,17 @@
 package com.example.ideationapp.Fragments;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.ideationapp.Model.UserModel;
@@ -13,14 +19,25 @@ import com.example.ideationapp.databinding.FragmentProfileFragmentBinding;
 import com.example.ideationapp.registerActivity;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class Profile_fragment extends Fragment {
 
     FragmentProfileFragmentBinding binding;
     FirebaseAuth fAuth;
     FirebaseFirestore fstore;
+    FirebaseDatabase database;
+    FirebaseStorage storage;
+    ActivityResultLauncher<String> launcher;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,12 +47,38 @@ public class Profile_fragment extends Fragment {
                 inflate(inflater, container, false);
         fAuth = FirebaseAuth.getInstance();
         fstore = FirebaseFirestore.getInstance();
+        database = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
+
+        launcher = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri uri) {
+                binding.userProfileImage.setImageURI(uri);
+                final StorageReference reference = storage.getReference().child("image");
+                reference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(getContext(), "Uploded", Toast.LENGTH_SHORT).show();
+                        reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+                                database.getReference().child("Users").child(fAuth.getCurrentUser().
+                                        getUid()).child("imageURL").setValue(uri.toString());
+                            }
+                        });
+                    }
+                });
+            }
+        });
 
         getUser();
 
-        if (fAuth.getCurrentUser().isEmailVerified())
-        binding.userName.setText("verified");
-        else binding.userName.setText("not verified");
+        binding.changeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launcher.launch("image/*");
+            }
+        });
 
         binding.logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,23 +94,29 @@ public class Profile_fragment extends Fragment {
 
     private void getUser() {
         String userID= fAuth.getCurrentUser().getUid();
-        fstore.collection("Users").document(userID).get().
-                addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+        database.getReference().child("Users").child(userID).addValueEventListener(new ValueEventListener() {
             @Override
-            public void onSuccess(DocumentSnapshot shot) {
-                UserModel user = shot.toObject(UserModel.class);
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                UserModel user = snapshot.getValue(UserModel.class);
                 if (user!=null) updateUser(user);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
 
+
+
     private void updateUser(UserModel user) {
         binding.constraintLayout2.setVisibility(View.VISIBLE);
         binding.progressBar4.setVisibility(View.GONE);
+        if (!user.getImageURL().equals("default")){
+            Picasso.get().load(user.getImageURL()).into(binding.userProfileImage);
+        }
         binding.userName.setText(user.getUserName());
         binding.workProfession.setText(user.getProfession());
         binding.followCount.setText(String.valueOf(user.getFollowCount()));
-        binding.totalHits.setText(String.valueOf(user.getTotalHits()));
-        binding.maxHits.setText(String.valueOf(user.getMaxHits()));
     }
 }
